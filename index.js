e3bd68f77eb769ec9e8279a23920d4f6c8724b63
Kuu <69802296+Kuuuuuuuu@users.noreply.github.com>
2022-06-19 22:13:06 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const {writeFileSync} = require("fs");
+const moment = require("moment");
+
+(function () {
+    const text = `<h3><b>🐱 Welcome to My Profile :3</b></h3>
+    <h4>📝 <b>Last Update ${moment().format('MMMM Do YYYY, h:mm:ss a')}</b></h4>
+<a href="https://Kuu.dripsquad.ga">
+    <img src="https://count.getloli.com/get/@MelidaZ?theme=moebooru"/>
+</a>
+<br>
+<br>
+<a href="https://Kuu.dripsquad.ga">
+    <img src="https://lanyard.cnrad.dev/api/568093374662311956"></a>
+</a>
+
+[![wakatime](https://wakatime.com/badge/user/f0797c6d-4099-4a7f-947c-a8144dcd6348.svg)](https://wakatime.com/@f0797c6d-4099-4a7f-947c-a8144dcd6348)
+
+#### Main Language: JS, TS, PHP, C#, C, JAVA, GO, Some SQL
+#### Web Development: HTML, CSS, React.js, Vue.js, Svelte
+#### Learning: Assembly, C++
+#### Tools: PHPStorm, Intellij IDEA Utimate, GoLand, CLion, Rider, WebStorm, PyCharm, Visual Studio 2022, Visual Studio Code, XAMPP, NeoVIM
+<h3>More Information</h3>
+<details>
+    <summary>Click Here!</summary>
+    <br>
+    <br>
+    <div align="center">
+        <img src="https://github-readme-stats.vercel.app/api?username=Kuuuuuuuu&show_icons=true&include_all_commits=true&line_height=28.5&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
+        <img src="https://github-readme-stats.vercel.app/api/top-langs?username=Kuuuuuuuu&langs_count=15&layout=compact&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
+        <br>
+        <img src="https://github-profile-trophy.vercel.app/?username=Kuuuuuuuu&row=2&column=4&theme=algolia"/>
+        <br>
+        <img src="https://github-readme-streak-stats.herokuapp.com/?user=Kuuuuuuuu&theme=dark&background=191970"/>
+        <br>
+        <br>
+        <img src="https://activity-graph.herokuapp.com/graph?username=Kuuuuuuuu&bg_color=191970&theme=github"/>
+        <br>
+        <br>
+        <img src="https://github-readme-stats.vercel.app/api/wakatime?username=Kuu&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"
+    </div>
+</details>`;
+    writeFileSync(path.join(__dirname, "README.md"), text);
+})();
