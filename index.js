609fe99359a10ac8ff5bd09eee82813a97e4bd5c
Nayuki <69802296+Kuuuuuuuu@users.noreply.github.com>
2022-08-27 00:12:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const path = require("path");
-const {writeFileSync} = require("fs");
+const { writeFileSync } = require("fs");
 const moment = require("moment");
 
 (function () {
     const text = `<h3><b>🐱 Welcome to My Profile :3</b></h3>
 <h4>📝 <b>Last Update: ${moment().format('MMMM Do YYYY, h:mm:ss a')}</b></h4>
-<a href="https://nayukikuu.gq">
-    <img src="https://count.getloli.com/get/@MelidaZ?theme=moebooru"/>
+<a href="https://nayukikuu.xyz">
+    <img src="https://count.getloli.com/get/@MelidaZ?theme=rule34"/>
 </a>
 <br>
 <br>
-<a href="https://nayukikuu.gq">
+<a href="https://nayukikuu.xyz">
     <img src="https://lanyard.cnrad.dev/api/568093374662311956"></a>
 </a>
 
 [![wakatime](https://wakatime.com/badge/user/f0797c6d-4099-4a7f-947c-a8144dcd6348.svg)](https://wakatime.com/@f0797c6d-4099-4a7f-947c-a8144dcd6348)
 
-#### Main Language: JS, TS, PHP, C#, C, JAVA, GO, Some SQL
-#### Web Development: HTML, CSS, React.js, Vue.js, Svelte
-#### Learning: Assembly, C++
-#### Tools: PHPStorm, Intellij IDEA Utimate, GoLand, CLion, Rider, WebStorm, PyCharm, Visual Studio 2022, Visual Studio Code, XAMPP, NeoVIM
-<h3>More Information</h3>
+#### Programming Languages: JS, TS, EJS, PHP, C#, C, JAVA, GO, Some SQL
+#### Frameworks: React.js, Vue.js, Express.js, Svelte, Bootstrap, Tailwind
+#### Learning: Elixir, C++, Flutter
+#### Tools: PhpStorm, Intellij Utimate, WebStorm, PyCharm, Rider, Vs Code(Insider) Git 
 <details>
-    <summary>Click Here!</summary>
+    <br>
+    <br>
+    <summary>More Info!</summary>
     <br>
     <br>
     <div align="center">
-        <a href="https://nayukikuu.gq">
+        <a href="https://nayukikuu.xyz">
            <img src="https://github-readme-stats.vercel.app/api?username=Kuuuuuuuu&show_icons=true&include_all_commits=true&line_height=28.5&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
            <img src="https://github-readme-stats.vercel.app/api/top-langs?username=Kuuuuuuuu&langs_count=15&layout=compact&count_private=true&title_color=82CAFF&icon_color=82CAFF&bg_color=191970&theme=nord"/>
            <br>
